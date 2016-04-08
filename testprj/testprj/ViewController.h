4fea2d90928919552536ba71a31fb6f706fa282a
snowkrash <sk@snowkrashs-MacBook-Pro.local>
2016-04-08 11:53:14 +0200

    init

---
+++ testprj/testprj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testprj
+//
+//  Created by snowkrash on 08.04.16.
+//  Copyright © 2016 snowkrash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
